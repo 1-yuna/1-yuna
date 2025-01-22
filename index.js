bf08e9e523899976963040ad63f78e8ce08112dc
yuna <micha0215@tukorea.ac.kr>
2025-01-22 17:37:48 +0900

    Update README.md

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ let text = `![header](https://capsule-render.vercel.app/api?type=waving&color=au
 <!--## 이런 환경에 익숙해요✍🏼-->
 
 ## 📚STACKS
-<img alt="" src= "https://img.shields.io/badge/java-white?logo=typescript&logoColor=red"/>
-<img alt="" src="https://img.shields.io/badge/spring-#6DB33F?style=for-the-badge&logo=Spring&logoColor=white">
+<img alt="" src= "https://img.shields.io/badge/java-white?logo=java&logoColor=red"/>
+<img alt="" src="https://img.shields.io/badge/spring-#6DB33F?style=for-the-badge&logo=spring&logoColor=white">
 <img alt="" src="https://img.shields.io/badge/mysql-#4479A1?style=for-the-badge&logo=mysql&logoColor=white">
 <img alt="" src="https://img.shields.io/badge/html5-#E34F26?style=for-the-badge&logo=html5&logoColor=white">
 <img alt="" src="https://img.shields.io/badge/css3-#1572B6?style=for-the-badge&logo=css3&logoColor=white">
