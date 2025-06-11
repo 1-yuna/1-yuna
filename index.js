f4ff55fece77332d9a410e32c72b689f906e5db1
yuna <micha0215@tukorea.ac.kr>
2025-06-12 02:56:27 +0900

    1-yuna

--- index.js
+++ index.js
@@ -27,8 +27,7 @@ let text = `![header](https://capsule-render.vercel.app/api?type=waving&color=au
 <!--  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> -->
 <!--  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>-->
 <!--</p>-->
-
-[![Yuna's GitHub stats](https://github-readme-stats.vercel.app/api?username=1-yuna)](https://github.com/anuraghazra/github-readme-stats)
+![Soohyeon's GitHub stats](https://github-readme-stats.vercel.app/api?username=1-yuna&show_icons=true&theme=light)
 
 ## 📕 Latest Blog Posts
 
